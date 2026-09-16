8db4279eed87b7bc84a187a162e27da5b04bb580
Apaar Raina <apaarraina@gmail.com>
2026-09-16 20:13:57 +0530

    added in FBPConvNet implementation

---
+++ difftomo/CT/Models/FBPConvNet.py
@@ -0,0 +1,98 @@
+#FBPConvNet implementation based on:
+
+# K. H. Jin et al.,
+# 'Deep Convolutional Neural Network for Inverse Problems in Imaging,'
+# IEEE Transactions on Image Processing, 2017.
+
+# Original implementation:
+# https://github.com/panakino/FBPConvNet
+
+
+
+import torch
+import torch.nn as nn
+
+
+class FBPConvNet(nn.Module):
+
+    def __init__(self, in_channels=1, out_channels=1):
+        super().__init__()
+
+        self.enc1 = self.conv_block(in_channels, 64)
+        self.enc2 = self.conv_block(64, 128)
+        self.enc3 = self.conv_block(128, 256)
+        self.enc4 = self.conv_block(256, 512)
+
+        self.bottleneck = self.conv_block(512, 1024)
+
+        self.up4 = nn.ConvTranspose2d(
+            1024, 512, kernel_size=2, stride=2
+        )
+        self.dec4 = self.conv_block(1024, 512)
+
+        self.up3 = nn.ConvTranspose2d(
+            512, 256, kernel_size=2, stride=2
+        )
+        self.dec3 = self.conv_block(512, 256)
+
+        self.up2 = nn.ConvTranspose2d(
+            256, 128, kernel_size=2, stride=2
+        )
+        self.dec2 = self.conv_block(256, 128)
+
+        self.up1 = nn.ConvTranspose2d(
+            128, 64, kernel_size=2, stride=2
+        )
+        self.dec1 = self.conv_block(128, 64)
+
+        self.out_conv = nn.Conv2d(
+            64, out_channels, kernel_size=1
+        )
+
+    @staticmethod
+    def conv_block(in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+
+        e1 = self.enc1(x)
+        p1 = nn.functional.max_pool2d(e1, 2)
+
+        e2 = self.enc2(p1)
+        p2 = nn.functional.max_pool2d(e2, 2)
+
+        e3 = self.enc3(p2)
+        p3 = nn.functional.max_pool2d(e3, 2)
+
+        e4 = self.enc4(p3)
+        p4 = nn.functional.max_pool2d(e4, 2)
+
+
+        b = self.bottleneck(p4)
+
+
+        u4 = self.up4(b)
+        u4 = torch.cat([u4, e4], dim=1)
+        d4 = self.dec4(u4)
+
+        u3 = self.up3(d4)
+        u3 = torch.cat([u3, e3], dim=1)
+        d3 = self.dec3(u3)
+
+        u2 = self.up2(d3)
+        u2 = torch.cat([u2, e2], dim=1)
+        d2 = self.dec2(u2)
+
+        u1 = self.up1(d2)
+        u1 = torch.cat([u1, e1], dim=1)
+        d1 = self.dec1(u1)
+
+        residual = self.out_conv(d1)
+
+        return x + residual
